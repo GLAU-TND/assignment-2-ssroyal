1ed7a8c9dc88c69eedf9c82f88c1690f6098ce1d
saurabh <saurabh.saraswat_cs18@gla.ac.in>
2020-03-24 10:06:40 +0530

    offer method of Queue but without ordering

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -8,8 +8,23 @@ package problem3.myqueue;
 
 import problem3.node.Node;
 
-public class MyPriorityQueue<E>{
+public class MyPriorityQueue<E> {
     private Node<E> front;
     private Node<E> rear ;
     private int size;
+    public boolean offer(E item) {
+        if(front==null)
+        {
+            rear= new Node<>(item);
+            front=rear;
+
+        }
+        else
+        {
+            rear.setNext(new Node<>(item));
+            rear=rear.getNext();
+        }
+        size++;
+        return true;
+    }
 }
