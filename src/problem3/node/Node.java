86a3404566aa34c6984e6c4772b26a2e16222026
saurabh <saurabh.saraswat_cs18@gla.ac.in>
2020-03-24 11:41:25 +0530

    toString method of Node class

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -35,4 +35,8 @@ public class Node<E> {
         this.data = data;
         this.next = next;
     }
+    @Override
+    public String toString() {
+        return this.getData().toString();
+    }
 }
