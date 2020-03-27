c540a596bbed219f26c09a8213e92d2a2848d712
saurabh <saurabh.saraswat_cs18@gla.ac.in>
2020-03-27 09:34:18 +0530

    Move Student class to problem 5 package

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -1,10 +1,52 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:06 PM
- */
 package problem5.student;
-// to store student information and properties
-public class Student {
+
+public class Student implements Comparable<Student> {
+    private int rollNo;
+    private String firstName;
+    private String lastName;
+    private String section;
+
+    public Student(int rollNo, String firstName, String lastName, String section) {
+        this.rollNo = rollNo;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.section = section;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    @Override
+    public int compareTo(Student student) {
+        return Integer.compare(this.rollNo, student.rollNo);
+    }
 }
