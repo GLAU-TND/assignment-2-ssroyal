eac95cfabf2de9fe23e150d8b0c29dd5084200e5
saurabh <saurabh.saraswat_cs18@gla.ac.in>
2020-03-28 15:51:12 +0530

    method for Prefix Order

--- src/problem2/main/Traverse/TraverseTree.java
+++ src/problem2/main/Traverse/TraverseTree.java
@@ -3,6 +3,20 @@ package problem2.main.Traverse;
 import problem1.node.TreeNode;
 
 public class TraverseTree {
+
+    void getPrefixOrder(TreeNode node) {
+
+        if (node == null)
+
+            return;
+
+        System.out.print(node.getData() + " ");
+
+        getPrefixOrder(node.getLeft());
+
+        getPrefixOrder(node.getRight());
+
+    }
     void getPostOrder(TreeNode node) {
 
         if (node == null)
