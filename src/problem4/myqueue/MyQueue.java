c6052774064ae8761ad41bc06e41d4909f1dcd75
saurabh <saurabh.saraswat_cs18@gla.ac.in>
2020-03-30 19:20:58 +0530

    a static node class

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -7,8 +7,6 @@
 package problem4.myqueue;
 // to create queue to store pre - order successor
 
-import problem5.node.Node;
-
 public class MyQueue {
     private Node front;
     private Node rear;
@@ -16,5 +14,27 @@ public class MyQueue {
     public MyQueue() {
         front = null;
         rear = null;
+    }
+
+    private static class Node {
+        int data;
+        Node next;
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
     }
 }
